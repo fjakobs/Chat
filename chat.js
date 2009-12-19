77a7f4878d3fad154fa46fc45ee5bcaec78d9d33
wittemann <martin.wittemann@1und1.de>
2009-12-19 20:15:25 +0100

    first commit

---
+++ chat.js
@@ -0,0 +1,9 @@
+var sys = require('sys'), 
+   http = require('http');
+
+http.createServer(function (req, res) {
+    
+  
+
+}).listen(8000);
+sys.puts('Server running at http://127.0.0.1:8000/');
