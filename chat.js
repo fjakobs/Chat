150d648efaf1b1093ecd2f88a17ece6b2f759506
wittemann <martin.wittemann@1und1.de>
2009-12-19 22:09:32 +0100

    Implemented the node.js server part.

--- chat.js
+++ chat.js
@@ -1,9 +1,26 @@
 var sys = require('sys'), 
    http = require('http');
 
+var resonses = [];
+
+
 http.createServer(function (req, res) {
-    
   
+  if (req.uri.params.connect) {
+    resonses.push(res);
+    return;
+  }
+
+  if (req.uri.params.post) {
+    for (var i = 0; i < resonses.length; i++) {
+      resonses[i].sendHeader(200, {'Content-Type': 'text/plain', 'Connection': 'close'});
+      resonses[i].sendBody(req.uri.params.post);
+      resonses[i].finish();
+    }
+    res.sendHeader(204, {'Connection': 'close'});
+    res.finish();
+    return;
+  }
 
 }).listen(8000);
 sys.puts('Server running at http://127.0.0.1:8000/');
